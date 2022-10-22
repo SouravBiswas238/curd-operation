ad323670db10211211455a8339c17d48c59062b4
SouravBiswas238 <souravbiswas427@gmail.com>
2022-10-22 17:20:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const app = express()
+const port = 5000;
+
+
+const corse = require('cors');
+require('dotenv').config();
+
+app.use(corse());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vkcbcgk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const articleCollection = client.db("Article").collection("Articles");
+        const articleDeleted = client.db("delete-articale").collection("deleted");
+
+        // post user information
+        app.post('/article', async (req, res) => {
+            const newTask = req.body;
+            const result = await articleCollection.insertOne(newTask);
+            res.send({ success: true, result });
+        });
+
+        // get all tasks
+        app.get('/article', async (req, res) => {
+            const query = {};
+            const cursor = articleCollection.find(query);
+            articles = await cursor.toArray();
+            res.send(articles);
+        });
+
+        // delete Single task
+        app.delete('/article/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await articleCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        app.put('/article/:id', async (req, res) => {
+            const id = req.params.id;
+            const editArticle = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: editArticle,
+            };
+            const result = await articleCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
