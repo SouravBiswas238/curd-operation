b24728f52b519f788c55a53d28f1ad9ac0405355
SouravBiswas238 <souravbiswas427@gmail.com>
2022-10-25 12:03:56 +0600

    adding url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vkcbcgk.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://curd-operation:YYD82hW5XPRgwr2H@cluster0.vkcbcgk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
